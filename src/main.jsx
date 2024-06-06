2723cdc6c32b707b91fcb2535c7f64e85be185c8
patelsweta2 <sweta0442@gmail.com>
2024-06-06 19:10:10 +0530

    implement okta authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { AppProvider } from "./context/ProductContext.jsx";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const domainName = import.meta.env.VITE_DOMAIN_NAME;
+const clientId = import.meta.env.VITE_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Auth0Provider
+      domain={domainName}
+      clientId={clientId}
+      authorizationParams={{
+        redirect_uri: window.location.origin,
+      }}
+    >
+      <AppProvider>
+        <App />
+      </AppProvider>
+    </Auth0Provider>
+  </React.StrictMode>
+);
