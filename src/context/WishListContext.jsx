6520e558431f7e318d3415927553579b02378e75
patelsweta2 <sweta0442@gmail.com>
2024-06-08 18:57:47 +0530

    wishListContext is complete

--- src/context/WishListContext.jsx
+++ src/context/WishListContext.jsx
@@ -1,7 +1,46 @@
-import React from "react";
+import { useEffect, useReducer, createContext, useContext } from "react";
+import reducer from "../reducers/wishListReducer";
 
-const WishListContext = () => {
-  return <div>WishListContext</div>;
+const wishListContext = createContext();
+
+const getLocalWishListItems = () => {
+  let localWishListData = localStorage.getItem("WishListCart");
+  const parsedData = JSON.parse(localWishListData);
+  if (!Array.isArray(parsedData)) return [];
+  return parsedData;
+};
+const initialState = {
+  wishListCart: getLocalWishListItems(),
 };
 
-export default WishListContext;
+export const WishlistContextProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const addToWishList = (product, amount = 1, addedToWishList = false) => {
+    return dispatch({
+      type: "ADD_TO_WISHLIST",
+      payload: { product, amount, addToWishList },
+    });
+  };
+  const removeFromWishList = (id) => {
+    return dispatch({ type: "REMOVE_FROM_WISHLIST", payload: id });
+  };
+  const clearWishList = () => {
+    console.log("clear wishlist");
+    return dispatch({ type: "CLEAR_WISHLIST" });
+  };
+  useEffect(() => {
+    localStorage.setItem("WishListCart", JSON.stringify(state.wishListCart));
+  }, [state.wishListCart]);
+
+  return (
+    <wishListContext.Provider
+      value={{ ...state, addToWishList, removeFromWishList, clearWishList }}
+    >
+      {children}
+    </wishListContext.Provider>
+  );
+};
+
+export const useWishListContext = () => {
+  return useContext(wishListContext);
+};
---
+++ src/reducers/wishListReducer.js
@@ -0,0 +1,43 @@
+const WishListReducer = (state, action) => {
+  switch (action.type) {
+    case "ADD_TO_WISHLIST":
+      let { product } = action.payload;
+      console.log(product);
+      let wishlistProduct;
+      wishlistProduct = {
+        id: product.id,
+        title: product.title,
+        thumbnail: product.thumbnail,
+        price: product.price,
+        description: product.description,
+        stock: product.stock,
+        brand: product.brand,
+        rating: product.rating,
+        discountPercentage: product.discountPercentage,
+      };
+      console.log(wishlistProduct);
+      return {
+        ...state,
+        wishlistProduct: [...state.wishListCart, wishlistProduct],
+      };
+    case "REMOVE_FROM_WISHLIST":
+      let updatedCart = state.wishListCart.filter(
+        (product) => product.id !== action.payload
+      );
+      return {
+        ...state,
+        wishListCart: updatedCart,
+      };
+    case "CLEAR_WISHLIST":
+      return {
+        ...state,
+        wishListCart: [],
+      };
+    default:
+      return {
+        ...state,
+      };
+  }
+};
+
+export default WishListReducer;
